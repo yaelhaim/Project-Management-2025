b035eccdb21ae9eb3e79a56b35a396d1414f17a1
Zohar-Monsonego <146588286+Zohar-Monsonego@users.noreply.github.com>
2024-12-31 19:51:02 +0200

    Adding DOM events with Java script

---
+++ productPage.js
@@ -0,0 +1,48 @@
+"use strict";
+
+//implementing what happens when clicking on the 'but now' button
+const button_buy_now = document.querySelector(".buy-now");
+console.log(button_buy_now);
+
+button_buy_now.addEventListener("click", () => {
+  alert(
+    "The purchase module is still under construction, thank you for your patience!"
+  );
+});
+
+//when clicking on the 'information brochure' link the brochure wiil be open
+function openPDF() {
+  var pdfOverlay = document.getElementById("pdf-overlay");
+  var canvas = document.getElementById("pdf-canvas");
+
+  if (!pdfOverlay || !canvas) {
+    console.error("One or more elements are missing!");
+    return;
+  }
+
+  pdfOverlay.style.display = "block";
+
+  pdfjsLib
+    .getDocument(pdfUrl)
+    .promise.then(function (pdf) {
+      pdf.getPage(1).then(function (page) {
+        var viewport = page.getViewport({ scale: 1 });
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        var renderContext = {
+          canvasContext: canvas.getContext("2d"),
+          viewport: viewport,
+        };
+        page.render(renderContext);
+      });
+    })
+    .catch(function (error) {
+      console.error("Error loading PDF:", error);
+    });
+}
+
+function closePDF() {
+  var pdfOverlay = document.getElementById("pdf-overlay");
+  pdfOverlay.style.display = "none";
+}
